d736651a4f7c7c4491cd3da8d4cb4497fb2d36b9
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2020-05-05 20:36:24 -0400

    Update webvm.ps1

--- scripts/webvm.ps1
+++ scripts/webvm.ps1
@@ -15,3 +15,17 @@ New-Item -ItemType Directory -Path C:\Temp -ErrorAction SilentlyContinue
 
 #Download Cosmos DB Emulator
 Invoke-WebRequest -Uri "https://aka.ms/cosmosdb-emulator" -OutFile "C:\Temp\cosmosdbemulator.exe"
+
+# Disable IE Enhanced Configuration
+function Disable-ieESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+Disable-ieESC
+
+# Disable Windows FireWall
+Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
