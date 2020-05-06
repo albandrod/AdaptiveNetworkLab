64d59a7c4767b73076b9b2882f1d6b40d5d9ae44
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2020-05-05 20:27:50 -0400

    Update webvm.ps1

--- scripts/webvm.ps1
+++ scripts/webvm.ps1
@@ -3,4 +3,15 @@ param (
     [string]$vmwebpip
 )
 
+# Install IIS
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
+# Modify listening port to 8089
+Set-WebBinding -Name 'Default Web Site' -BindingInformation "*:80:" -PropertyName Port -Value 8089
+
+
+# Create temp directory for downloaded files
+New-Item -ItemType Directory -Path C:\Temp -ErrorAction SilentlyContinue
+
+#Download Cosmos DB Emulator
+Invoke-WebRequest -Uri "https://aka.ms/cosmosdb-emulator" -OutFile "C:\Temp\cosmosdbemulator.exe"
