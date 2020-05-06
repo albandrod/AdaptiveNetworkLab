666aadc428afb065057b5bcd2b49c9a1895e5a1b
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2020-05-05 20:36:54 -0400

    Update datavm.ps1

--- scripts/datavm.ps1
+++ scripts/datavm.ps1
@@ -10,6 +10,20 @@ New-Item -ItemType Directory -Path C:\Temp -ErrorAction SilentlyContinue
 #download FileZilla Server Setup
 Invoke-WebRequest -Uri "https://dl3.cdn.filezilla-project.org/server/FileZilla_Server-0_9_60_2.exe?h=-bHapoYzS9xZhsW53Bi1dg&x=1588716262" -OutFile "C:\Temp\filezillaserver.exe"
 
+# Disable IE Enhanced Configuration
+function Disable-ieESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+Disable-ieESC
+
+# Disable Windows FireWall
+Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
+
 cd /
 
 # Install FileZilla Server
